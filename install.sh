904e0208797c23afd13dddae0661bbcd9a946e24
davidokubanjo <68503538+davidokubanjo@users.noreply.github.com>
2022-11-28 21:57:56 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
-!#/bin/bash
+testing 1 2 3
 
-yum install httpd -y
-firewall-cmd --permanent --add-port=80/tcp
-firewall-cmd --reload
-systemctl enable --now httpd
-echo "IT is UP" > /var/www/html/index.html
+
+# !#/bin/bash
+
+
+#yum install httpd -y
+#firewall-cmd --permanent --add-port=80/tcp
+#firewall-cmd --reload
+#systemctl enable --now httpd
+
+#echo "IT is UP" > /var/www/html/index.html
